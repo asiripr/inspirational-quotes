5545063c000a47b16810c701321f63f71dcc2a2d
Asiri Pramodaya <asiripramoday2000@gmail.com>
2024-06-10 12:06:02 +0530

    created the quote show area

--- lib/inspiring.dart
+++ lib/inspiring.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -20,73 +22,81 @@ class _InspiringState extends State<Inspiring> {
       appBar: AppBar(title: const Text("Inspirational Quotes"), backgroundColor: Colors.green.shade200,),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: quoteController,
-              decoration: const InputDecoration(
-                label: Text("Quote")
-              ),
-            ),
-            const SizedBox(height: 30,),
-            TextField(
-              controller: autherController,
-              decoration: const InputDecoration(
-                label: Text("Author")
-              ),
-            ),
-            const SizedBox(height: 30,),
-            
-            ElevatedButton(
-              onPressed: (){
-                CollectionReference collRef = FirebaseFirestore.instance.collection('quotes');
-                collRef.add({
-                  'quote': quoteController.text,
-                  'author':autherController.text
-                });
-                quoteController.clear();
-                autherController.clear();
-              },
-              child: Container(
-                width: 80,
-                height: 30,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5)
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                controller: quoteController,
+                decoration: const InputDecoration(
+                  label: Text("Quote")
                 ),
-                child: const Center(child: Text("Save")),
               ),
-            ),
-            const SizedBox(height: 30,),
-            ElevatedButton(
-              onPressed: () async{
-                CollectionReference collRef = FirebaseFirestore.instance.collection('quotes');
-                QuerySnapshot querySnapshot = await collRef.get();
-                if (querySnapshot.docs.isNotEmpty) {
-                  var doc = querySnapshot.docs[0];
-                  setState(() {
-                    receivedQuote = doc['quote'];
-                    receivedAuthor = doc['author'];
-                    showCard();
-                  });    
-                }
-              },
-              child: Container(
-                width: 80,
-                height: 30,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5)
+              const SizedBox(height: 30,),
+              TextField(
+                controller: autherController,
+                decoration: const InputDecoration(
+                  label: Text("Author")
                 ),
-                child: const Center(child: Text("Recieve")),
               ),
-            ),
-            if(receivedQuote.isNotEmpty){
+              const SizedBox(height: 30,),
               
-            }
-          ],
+              ElevatedButton(
+                onPressed: (){
+                  CollectionReference collRef = FirebaseFirestore.instance.collection('quotes');
+                  collRef.add({
+                    'quote': quoteController.text,
+                    'author':autherController.text
+                  });
+                  quoteController.clear();
+                  autherController.clear();
+                },
+                child: Container(
+                  width: 80,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5)
+                  ),
+                  child: const Center(child: Text("Save")),
+                ),
+              ),
+              const SizedBox(height: 15,),
+              ElevatedButton(
+                onPressed: () async{
+                  CollectionReference collRef = FirebaseFirestore.instance.collection('quotes');
+                  QuerySnapshot querySnapshot = await collRef.get();
+                  if (querySnapshot.docs.isNotEmpty) {
+                    Random random = Random();
+                    int randomIndex = random.nextInt(querySnapshot.docs.length);
+                    var doc = querySnapshot.docs[randomIndex];
+                    setState(() {
+                      receivedQuote = doc['quote'];
+                      receivedAuthor = doc['author']; 
+                    });    
+                  }
+                  
+                },
+                child: Container(
+                  width: 80,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5)
+                  ),
+                  child: const Center(child: Text("Recieve")),
+                ),
+              ),
+              const SizedBox(height: 20,),
+              Text(
+                receivedQuote.isNotEmpty ? 'Quote: \n$receivedQuote \n\nAuthor:\n$receivedAuthor' : '',
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
         ),
       ),
     );
   }
   
-  void showCard() {}
 }
